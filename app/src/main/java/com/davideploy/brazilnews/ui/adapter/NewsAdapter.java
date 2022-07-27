160bcdeeb29eefb30197bcc2c84a4fd3f589f33b
davideploy <deehzi@pm.me>
2022-07-27 16:16:44 -0300

    adicionando listener no botão de favoritar e função no botão de compartilhar noticia.

--- app/src/main/java/com/davideploy/brazilnews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/com/davideploy/brazilnews/ui/adapter/NewsAdapter.java
@@ -3,6 +3,7 @@ package com.davideploy.brazilnews.ui.adapter;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -16,10 +17,12 @@ import java.util.List;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
-    private List<News> news;
+    private final List<News> news;
+    private final View.OnClickListener favoriteListener;
 
-    public NewsAdapter(List<News> news) {
+    public NewsAdapter(List<News> news, View.OnClickListener favoriteListener) {
         this.news = news;
+        this.favoriteListener = favoriteListener;
     }
 
     @NonNull
@@ -33,17 +36,28 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position){
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvDescription.setText((news.getDescription()));
-        Picasso.get().load(news.getImage())
-                .fit()
-                .into(holder.binding.ivThumbnail);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvDescription.setText((news.description));
+        Picasso.get().load(news.image).fit().into(holder.binding.ivThumbnail);
+
+        // Abrir link da noticia
         holder.binding.btOpenLink.setOnClickListener(view -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
+            i.setData(Uri.parse(news.link));
             holder.itemView.getContext().startActivity(i);
 
         });
+
+        // Compartilhar noticia
+        holder.binding.ivShare.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/pain");
+            i.putExtra(Intent.EXTRA_TEXT, news.link);
+            holder.itemView.getContext().startActivity(Intent.createChooser(i, "Share via"));
+        });
+
+        // Favoritar noticia, instanciado pelo Fragment
+        holder.binding.ivFavorite.setOnClickListener(this.favoriteListener);
     }
 
     @Override
