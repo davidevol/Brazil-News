e3838247fd4655c76913bdbb61b7f2cd7f56e2ce
davideploy <deehzi@pm.me>
2022-08-04 16:13:46 -0300

    adicionando um observador de noticias e correções menores

--- app/src/main/java/com/davideploy/brazilnews/data/remote/BrazilNewsApi.java
+++ app/src/main/java/com/davideploy/brazilnews/data/remote/BrazilNewsApi.java
@@ -7,7 +7,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface SoccerNewsApi {
+public interface BrazilNewsApi {
 
     @GET("news.json")
     Call<List<News>> getNews();
--- app/src/main/java/com/davideploy/brazilnews/ui/news/NewsFragment.java
+++ app/src/main/java/com/davideploy/brazilnews/ui/news/NewsFragment.java
@@ -10,22 +10,53 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import com.davideploy.brazilnews.R;
 import com.davideploy.brazilnews.databinding.FragmentNewsBinding;
-import com.davideploy.brazilnews.ui.adapter.NewsAdapter;
+import com.davideploy.brazilnews.ui.adapters.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private NewsViewModel newsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
+        newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
-        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> binding.rvNews.setAdapter(new NewsAdapter(news)));
+
+        observeNews();
+        observeStates();
+
+        binding.srlNews.setOnRefreshListener(newsViewModel::findNews);
+
         return root;
+    }
+
+    private void observeNews() {
+        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
+            binding.rvNews.setAdapter(new NewsAdapter(news, newsViewModel::saveNews));
+        });
+    }
+
+    private void observeStates() {
+        newsViewModel.getState().observe(getViewLifecycleOwner(), state -> {
+            switch (state) {
+                case DOING:
+                    binding.srlNews.setRefreshing(true);
+                    break;
+                case DONE:
+                    binding.srlNews.setRefreshing(false);
+                    break;
+                case ERROR:
+                    binding.srlNews.setRefreshing(false);
+                    Snackbar.make(binding.srlNews, R.string.error_network, Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
